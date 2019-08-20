bb4866be6b963bd467142919d93ab2a5b66ecec8
Autodidax86 <martin.muehlenberg@googlemail.com>
2019-08-20 13:12:30 +0200

    fixing spinning wheel progress bar

--- src/main/java/com/autodidax/demoncycle/blocks/Gui/GuiSpinningWheel.java
+++ src/main/java/com/autodidax/demoncycle/blocks/Gui/GuiSpinningWheel.java
@@ -9,8 +9,8 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 
-public class GuiSpinningWheel extends GuiContainer {
-
+public class GuiSpinningWheel extends GuiContainer 
+{
 	private static final ResourceLocation TEXTURES = new ResourceLocation(Reference.MOD_ID + ":textures/gui/spinning_wheel.png");
 	private final InventoryPlayer player;
 	private final TileEntitySpinningWheel tileentity;
@@ -36,28 +36,14 @@ public class GuiSpinningWheel extends GuiContainer {
 		GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 		this.mc.getTextureManager().bindTexture(TEXTURES);
 		this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
-		
-//		if(TileEntitySpinningWheel.isWorking(tileentity))
-//		{
-//			int k = this.getBurnLeftScaled(13);
-//			this.drawTexturedModalRect(this.guiLeft + 8, this.guiTop + 54 + 12 - k, 176, 12 - k, 14, k + 1);
-//		}
-		
 		int l = this.getSpinningProgressScaled(24);
-		this.drawTexturedModalRect(this.guiLeft + 44, this.guiTop + 36, 176, 14, l + 1, 16);
+		this.drawTexturedModalRect(this.guiLeft + 80, this.guiTop + 36, 176, 14, l + 1, 16);
 	}
-	
-//	private int getBurnLeftScaled(int pixels)
-//	{
-//		int i = this.tileentity.getField(1);
-//		if(i == 0) i = 200;
-//		return this.tileentity.getField(0) * pixels / i;
-//	}
 	
 	private int getSpinningProgressScaled(int pixels)
 	{
 		int i = this.tileentity.getField(0);
-		int j = this.tileentity.getField(2);
+		int j = this.tileentity.getField(1);
 		return j != 0 && i != 0 ? i * pixels / j : 0;
 	}
 }
