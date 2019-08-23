2a32dfe9cbf00a481c7e52ad228e277187b1291b
Autodidax86 <martin.muehlenberg@googlemail.com>
2019-08-23 10:29:53 +0200

    rename MOD_ID from dcm to demoncycle

--- src/main/java/com/autodidax/demoncycle/util/Reference.java
+++ src/main/java/com/autodidax/demoncycle/util/Reference.java
@@ -6,7 +6,7 @@ import net.minecraft.item.ItemStack;
 public class Reference {
 
 	public static final CreativeTabs DemonCycleTab = new DemonCycleTab();
-	public static final String MOD_ID = "dcm";
+	public static final String MOD_ID = "demoncycle";
 	public static final String NAME = "Demoncycle Mod";
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "(1.12.2)";
